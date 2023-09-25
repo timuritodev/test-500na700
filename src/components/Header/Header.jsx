4aa762c0f1a606538863629f81e98c47a6c4de8c
luciD <sosalnet@yandex.ru>
2023-09-26 02:41:03 +0300

    made half header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-// import "./Header.css";
+import React from "react";
+import "./Header.scss";
+import pic from "../../assets/logo.svg"
+
 
 export const Header = () => {
   return (
-    <footer className="footer">
-      {/* <img className="header__logo" src={pic} alt={pic} /> */}
-      <h2>привет</h2>
-    </footer>
+    <header className="header">
+      <img className="header__logo" src={pic} alt={pic} />
+      <ul className="header__container">
+        <li className="header__link">Пункт 1</li>
+        <li className="header__link">Пункт 2</li>
+        <li className="header__link">Пункт 3</li>
+        <li className="header__link">Пункт 4</li>
+        <li className="header__link">Пункт 5</li>
+      </ul>
+    </header>
   );
 };
