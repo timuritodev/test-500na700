9938c1795e0aa6f8eb878c2748247deb5cd36458
luciD <sosalnet@yandex.ru>
2023-09-26 10:52:08 +0300

    done with footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import pic from "../../assets/logo.svg"
-// import "./Header.css";
+import React from "react";
+import fb from "../../assets/fb-logo.svg";
+import tt from "../../assets/tt-logo.svg";
+import yt from "../../assets/yt-logo.svg";
+import vk from "../../assets/vk-logo.svg";
+
+import "./Footer.scss";
 
 export const Footer = () => {
   return (
     <footer className="footer">
-      <img className="header__logo" src={pic} alt={pic} />
-      <h2>пика</h2>
+      <div className="footer__description">
+        <h2 className="footer__title">Заголовок</h2>
+        <p className="footer__subtitle">
+          ОписаниОписание Описание Описание Описание Описание Описаниее
+        </p>
+        <a className="footer__link" href="500na700.ru">500na700.ru</a>
+      </div>
+      <div className="image__container">
+        <img className="img" src={vk} alt="" />
+        <img className="img" src={fb} alt="" />
+        <img className="img" src={tt} alt="" />
+        <img className="img" src={yt} alt="" />
+      </div>
     </footer>
   );
 };
