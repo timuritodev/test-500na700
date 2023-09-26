d66d3995866cb926dbff5b8bba72a1fcfc52335b
luciD <sosalnet@yandex.ru>
2023-09-26 23:13:17 +0300

    added fix on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.scss";
 import pic from "../../assets/logo.svg";
 
 export const Header = () => {
+  const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsHeaderFixed(true);
+      } else {
+        setIsHeaderFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className={`header ${isHeaderFixed ? "fixed" : ""}`}>
       <div className="header__container">
         <img className="header__logo" src={pic} alt={pic} />
         <ul className="header__links-container">
