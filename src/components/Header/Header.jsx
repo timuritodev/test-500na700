45262efd085cf9b0aa2cdfd620e0669a654c0c6d
luciD <sosalnet@yandex.ru>
2023-09-27 17:22:54 +0300

    header popup in progress

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,15 @@ import pic from "../../assets/logo.svg";
 
 export const Header = () => {
   const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const setNavOpen = () => {
+    setIsOpen(true);
+  };
+
+  const setNavClose = () => {
+    setIsOpen(false);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -23,10 +32,21 @@ export const Header = () => {
 
   return (
     <header className={`header ${isHeaderFixed ? "fixed" : ""}`}>
+      <nav
+        className={`header__content ${isOpen && "header__content_open"}`}
+        onMouseOver={setNavOpen}
+        onMouseOut={setNavClose}
+      >
+        <ul className="header__list" onMouseOver={setNavOpen}>
+          <p className="">gbhs</p>
+        </ul>
+      </nav>
       <div className="header__container">
         <img className="header__logo" src={pic} alt={pic} />
         <ul className="header__links-container">
-          <li className="header__link">Пункт 1</li>
+          <li className="header__link" onMouseOver={setNavOpen}>
+            Пункт 1
+          </li>
           <li className="header__link">Пункт 2</li>
           <li className="header__link">Пункт 3</li>
           <li className="header__link">Пункт 4</li>
