6acfe384bebe7b58696c7da4077930f7b2ff3481
luciD <sosalnet@yandex.ru>
2023-09-27 21:20:03 +0300

    almost done with header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,56 +1,88 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Header.scss";
 import pic from "../../assets/logo.svg";
 
 export const Header = () => {
-  const [isHeaderFixed, setIsHeaderFixed] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
-
-  const setNavOpen = () => {
-    setIsOpen(true);
-  };
-
-  const setNavClose = () => {
-    setIsOpen(false);
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setIsHeaderFixed(true);
-      } else {
-        setIsHeaderFixed(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <header className={`header ${isHeaderFixed ? "fixed" : ""}`}>
-      <nav
-        className={`header__content ${isOpen && "header__content_open"}`}
-        onMouseOver={setNavOpen}
-        onMouseOut={setNavClose}
-      >
-        <ul className="header__list" onMouseOver={setNavOpen}>
-          <p className="">gbhs</p>
-        </ul>
-      </nav>
+    <header className={"header"}>
       <div className="header__container">
         <img className="header__logo" src={pic} alt={pic} />
         <ul className="header__links-container">
-          <li className="header__link" onMouseOver={setNavOpen}>
+          <li className="header__link">
             Пункт 1
+            <ul className="header__sub-menu">
+              <li>
+                <a className="header__sub-link" href="#">
+                  Подпункт меню 1
+                </a>
+              </li>
+              <li>
+                <a className="header__sub-link" href="#">
+                  Подпункт меню 1
+                </a>
+              </li>
+            </ul>
           </li>
-          <li className="header__link">Пункт 2</li>
-          <li className="header__link">Пункт 3</li>
-          <li className="header__link">Пункт 4</li>
-          <li className="header__link">Пункт 5</li>
+          <li className="header__link">
+            Пункт 2
+            <ul className="header__sub-menu">
+              <li>
+                <a className="header__sub-link" href="#">
+                  Подпункт меню 2
+                </a>
+              </li>
+              <li>
+                <a className="header__sub-link" href="#">
+                  Подпункт меню 2
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="header__link">
+            Пункт 3
+            <ul className="header__sub-menu">
+              <li>
+                <a className="header__sub-link" href="#">
+                  Подпункт меню 3
+                </a>
+              </li>
+              <li>
+                <a className="header__sub-link" href="#">
+                  Подпункт меню 3
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="header__link">
+            Пункт 4
+            <ul className="header__sub-menu">
+              <li>
+                <a className="header__sub-link" href="#">
+                  Подпункт меню 4
+                </a>
+              </li>
+              <li>
+                <a className="header__sub-link" href="#">
+                  Подпункт меню 4
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="header__link">
+            Пункт 5
+            <ul className="header__sub-menu">
+              <li>
+                <a className="header__sub-link" href="#">
+                  Подпункт меню 5
+                </a>
+              </li>
+              <li>
+                <a className="header__sub-link" href="#">
+                  Подпункт меню 5
+                </a>
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
     </header>
