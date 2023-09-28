3a53367957a2bf96a62bfc8c586c6a37746e42b9
luciD <sosalnet@yandex.ru>
2023-09-29 00:55:59 +0300

    fixed adaptive 7.0

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,83 +1,83 @@
 import React from 'react';
 // import { FC, useState, useEffect } from 'react';
 import "./Popup.scss";
-import "../Header/Header.scss"
+// import "../popup/popup.scss"
 
 export const Popup = ({ isPopupOpen, switchPopup }) => {
   return (
     <div className={`popup ${isPopupOpen ? "popup_opened" : ""}`}>
       <div className="popup__content">
       <ul className="popup__links-container">
-          <li className="header__link">
+          <li className="popup__link">
             Пункт 1
-            <ul className="header__sub-menu">
+            <ul className="popup__sub-menu">
               <li>
-                <a className="header__sub-link" href="#">
+                <a className="popup__sub-link" href="#">
                   Подпункт меню 1
                 </a>
               </li>
               <li>
-                <a className="header__sub-link" href="#">
+                <a className="popup__sub-link" href="#">
                   Подпункт меню 1
                 </a>
               </li>
             </ul>
           </li>
-          <li className="header__link">
+          <li className="popup__link">
             Пункт 2
-            <ul className="header__sub-menu">
+            <ul className="popup__sub-menu">
               <li>
-                <a className="header__sub-link" href="#">
+                <a className="popup__sub-link" href="#">
                   Подпункт меню 2
                 </a>
               </li>
               <li>
-                <a className="header__sub-link" href="#">
+                <a className="popup__sub-link" href="#">
                   Подпункт меню 2
                 </a>
               </li>
             </ul>
           </li>
-          <li className="header__link">
+          <li className="popup__link">
             Пункт 3
-            <ul className="header__sub-menu">
+            <ul className="popup__sub-menu">
               <li>
-                <a className="header__sub-link" href="#">
+                <a className="popup__sub-link" href="#">
                   Подпункт меню 3
                 </a>
               </li>
               <li>
-                <a className="header__sub-link" href="#">
+                <a className="popup__sub-link" href="#">
                   Подпункт меню 3
                 </a>
               </li>
             </ul>
           </li>
-          <li className="header__link">
+          <li className="popup__link">
             Пункт 4
-            <ul className="header__sub-menu">
+            <ul className="popup__sub-menu">
               <li>
-                <a className="header__sub-link" href="#">
+                <a className="popup__sub-link" href="#">
                   Подпункт меню 4
                 </a>
               </li>
               <li>
-                <a className="header__sub-link" href="#">
+                <a className="popup__sub-link" href="#">
                   Подпункт меню 4
                 </a>
               </li>
             </ul>
           </li>
-          <li className="header__link">
+          <li className="popup__link">
             Пункт 5
-            <ul className="header__sub-menu">
+            <ul className="popup__sub-menu">
               <li>
-                <a className="header__sub-link" href="#">
+                <a className="popup__sub-link" href="#">
                   Подпункт меню 5
                 </a>
               </li>
               <li>
-                <a className="header__sub-link" href="#">
+                <a className="popup__sub-link" href="#">
                   Подпункт меню 5
                 </a>
               </li>
